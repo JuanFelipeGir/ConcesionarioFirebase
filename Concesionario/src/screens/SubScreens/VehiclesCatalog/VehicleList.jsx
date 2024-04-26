8290d1c918e293abfc6b4cc03fe8f1ac24426895
MELANNY <101124668+melanny0822@users.noreply.github.com>
2024-04-26 14:39:21 -0500

    style:refactor styles for component catalog

--- Concesionario/src/screens/SubScreens/VehiclesCatalog/VehicleList.jsx
+++ Concesionario/src/screens/SubScreens/VehiclesCatalog/VehicleList.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { ScrollView} from 'react-native'
+import { ScrollView } from 'react-native'
 import Vehicle from './Vehicle'
 
-const VehicleList = (vehicles) => {
+const VehicleList = ({ vehicles }) => {
   return (
     <ScrollView>
-        {vehicles.map((vehicle)=>(
-            <Vehicle
-                key={vehicle.id}
-                imageUrl={vehicle.imageUrl}
-                description={vehicle.description}
-                price={vehicle.price}
-            />
-  ))}
+      {vehicles.map((vehicle) => (
+        <Vehicle
+          key={vehicle.id}
+          imageUrl={vehicle.imageUrl}
+          description={vehicle.description}
+          price={vehicle.price}
+        />
+      ))}
     </ScrollView>
   )
 }
 
 export default VehicleList
+
